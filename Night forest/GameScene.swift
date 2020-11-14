e628413b79dadc282ed7de08e781eae415e3c26e
Tuiaara <tuyara_ok@mail.ru>
2020-11-14 15:02:03 +0300

    Настроила смерть персонажа, добавила класс animationClass, метод shakeAndFlash, reloadGame

--- Night forest/GameScene.swift
+++ Night forest/GameScene.swift
@@ -10,10 +10,23 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    // Variables
+    var sound = true
+    var moveBatY = SKAction()
+    var gameViewControllerBridge: GameViewController!
+    var death = false
+    var animations = AnimationClass()
+    
     // TEXTURES
     var bgTexture: SKTexture!
     var flyHeroTex: SKTexture!
     var runHeroTex: SKTexture!
+    var coinTex: SKTexture!
+    var redCoinTex: SKTexture!
+    var coinHeroTex: SKTexture!
+    var redCoinHeroTex: SKTexture!
+    var batTex: SKTexture!
+    var deadHeroTex: SKTexture!
     
     // Emitters Node
     var heroEmitter = SKEmitterNode()
@@ -26,22 +39,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var ground = SKSpriteNode()
     var sky = SKSpriteNode()
     var hero = SKSpriteNode()
+    var coin = SKSpriteNode()
+    var redCoin = SKSpriteNode()
+    var bat = SKSpriteNode()
     
     
     // Sprites Objects
     var bgObject = SKNode()
     var groundObject = SKNode()
     var heroObject = SKNode()
-    var skyObject = SKNode()
+    var movingObject = SKNode()
     var heroemitterObject = SKNode()
+    var coinObject = SKNode()
+    var redCoinObject = SKNode()
     
     // Bit masks
     var heroGroup: UInt32 = 0x1 << 1
     var groundGroup: UInt32 = 0x1 << 2
+    var coinGroup: UInt32 = 0x1 << 3
+    var redCoinGroup: UInt32 = 0x1 << 4
+    var objectGroup: UInt32 = 0x1 << 5
     
     // Textures array for animateWithTexture
     var heroflyTexturesArray = [SKTexture]()
     var heroRunTexturesArray = [SKTexture]()
+    var coinTexturesArray = [SKTexture]()
+    var batTexturesArray = [SKTexture]()
+    var heroDeathTexturesArray = [SKTexture]()
+    
+    // Timers
+    var timerAddCoin = Timer()
+    var timerAddRedCoin = Timer()
+    var timerAddBoneTimer = Timer()
+    
+    // Sounds
+    var pickCoinPreload = SKAction()
+    var batCreatePreload = SKAction()
+    var batDeadPreload = SKAction()
     
     override func didMove(to view: SKView) {
         // Background texture
@@ -51,6 +85,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         flyHeroTex = SKTexture(imageNamed: "jump.png")
         runHeroTex = SKTexture(imageNamed: "r_000.png")
         
+        // Coin texture
+        coinTex = SKTexture(imageNamed: "coin.jpg")
+        redCoinTex = SKTexture(imageNamed: "coin.jpg")
+        coinHeroTex = SKTexture(imageNamed: "Coin0.png")
+        redCoinHeroTex = SKTexture(imageNamed: "Coin0.jpg")
+        
+        // Bone texture
+        batTex = SKTexture(imageNamed: "bat-1.png")
+        
         // Emitters
         heroEmitter = SKEmitterNode(fileNamed: "engine.sks")!
         
@@ -58,14 +101,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         createObjects()
         createGame()
+        
+        pickCoinPreload = SKAction.playSoundFileNamed("pickCoin.mp3", waitForCompletion: false)
+        batCreatePreload = SKAction.playSoundFileNamed("bat.mp3", waitForCompletion: false)
+        batDeadPreload = SKAction.playSoundFileNamed("batDeath2.mp3", waitForCompletion: false)
     }
     
     func createObjects() {
         self.addChild(bgObject)
         self.addChild(groundObject)
-        self.addChild(skyObject)
+        self.addChild(movingObject)
         self.addChild(heroObject)
         self.addChild(heroemitterObject)
+        self.addChild(coinObject)
+        self.addChild(redCoinObject)
     }
     
     func createGame() {
@@ -73,8 +122,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createGround()
         createSky()
         
-        createHero()
-        createHeroemitter()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.createHero()
+            self.createHeroemitter()
+            self.timerFunc()
+            self.addBat()
+        }
+        
+        gameViewControllerBridge.refreshGameButton.isHidden = true
     }
     
     func createBg() {
@@ -112,7 +167,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         sky.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.size.width + 100, height: self.frame.size.height - 50))
         sky.physicsBody?.isDynamic = false
         sky.zPosition = 1
-        skyObject.addChild(sky)
+        movingObject.addChild(sky)
     }
     
     func addHero(heroNode: SKSpriteNode, atPosition position: CGPoint) {
@@ -132,7 +187,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         hero.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: hero.size.width - 40, height: hero.size.height - 30))
         
         hero.physicsBody?.categoryBitMask = heroGroup
-        hero.physicsBody?.contactTestBitMask = groundGroup
+        hero.physicsBody?.contactTestBitMask = groundGroup | coinGroup | redCoinGroup | objectGroup
         hero.physicsBody?.collisionBitMask = groundGroup
         
         hero.physicsBody?.isDynamic = true
@@ -150,6 +205,169 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         heroEmitter = SKEmitterNode(fileNamed: "engine.sks")!
         heroemitterObject.zPosition = 1
         heroemitterObject.addChild(heroEmitter)
+    }
+    
+    @objc func addCoin() {
+        coin = SKSpriteNode(texture: coinTex)
+        coinTexturesArray = [SKTexture(imageNamed: "Coin0.png"), SKTexture(imageNamed: "Coin1.png"), SKTexture(imageNamed: "Coin2.png"), SKTexture(imageNamed: "Coin3.png")]
+        let coinAnimation = SKAction.animate(with: coinTexturesArray, timePerFrame: 0.1)
+        let coinHero = SKAction.repeatForever(coinAnimation)
+        coin.run(coinHero)
+        
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        coin.size.width = 40
+        coin.size.height = 40
+        coin.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: coin.size.width - 20, height: coin.size.height - 20))
+        coin.physicsBody?.restitution = 0
+        coin.position = CGPoint(x: self.size.width + 50, y: 0 + coinTex.size().height + 90 + pipeOffset)
+        
+        let moveCoin = SKAction.moveBy(x: -self.frame.size.width * 2, y: 0, duration: 5)
+        let removeAction = SKAction.removeFromParent()
+        let coinMoveBGForever = SKAction.repeatForever(SKAction.sequence([moveCoin, removeAction]))
+        coin.run(coinMoveBGForever)
+        
+        coin.physicsBody?.isDynamic = false
+        coin.physicsBody?.categoryBitMask = coinGroup
+        coin.zPosition = 1
+        coinObject.addChild(coin)
+    }
+    
+    @objc func redCoinAdd() {
+        redCoin = SKSpriteNode(texture: redCoinTex)
+        coinTexturesArray = [SKTexture(imageNamed: "Coin0.png"), SKTexture(imageNamed: "Coin1.png"), SKTexture(imageNamed: "Coin2.png"), SKTexture(imageNamed: "Coin3.png")]
+        let redCoinAnimation = SKAction.animate(with: coinTexturesArray, timePerFrame: 0.1)
+        let redCoinHero = SKAction.repeatForever(redCoinAnimation)
+        redCoin.run(redCoinHero)
+        
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        redCoin.size.width = 40
+        redCoin.size.height = 40
+        redCoin.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: redCoin.size.width - 10, height: redCoin.size.height - 10))
+        redCoin.physicsBody?.restitution = 0
+        redCoin.position = CGPoint(x: self.size.width + 50, y: 0 + redCoinTex.size().height + 90 + pipeOffset)
+               
+        let moveCoin = SKAction.moveBy(x: -self.frame.size.width * 2, y: 0, duration: 5)
+        let removeAction = SKAction.removeFromParent()
+        let coinMoveBGForever = SKAction.repeatForever(SKAction.sequence([moveCoin, removeAction]))
+        redCoin.run(coinMoveBGForever)
+               
+        redCoin.setScale(1.3)
+        redCoin.physicsBody?.isDynamic = false
+        redCoin.physicsBody?.categoryBitMask = redCoinGroup
+        redCoin.zPosition = 1
+        redCoinObject.addChild(redCoin)
+    }
+    
+    @objc func addBat() {
+        if sound {
+            run(batCreatePreload)
+        }
+        
+        bat = SKSpriteNode(texture: batTex)
+        batTexturesArray = [SKTexture(imageNamed: "bat-1.png"), SKTexture(imageNamed: "bat-2.png"), SKTexture(imageNamed: "bat-3.png"), SKTexture(imageNamed: "bat-4.png"), SKTexture(imageNamed: "bat-5.png")]
+        let batAnimation = SKAction.animate(with: batTexturesArray, timePerFrame: 0.03)
+        let batHero = SKAction.repeatForever(batAnimation)
+        bat.run(batHero)
+        
+        let randomPosition = arc4random() % 2
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 5)
+        let pipeOffset = self.frame.size.height / 4 + 30 - CGFloat(movementAmount)
+        
+        if randomPosition == 0 {
+            bat.position = CGPoint(x: self.size.width + 50, y: 0 + batTex.size().height / 2 + 90 + pipeOffset)
+            bat.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: bat.size.width - 40, height: bat.size.height - 20))
+        } else {
+            bat.position = CGPoint(x: self.size.width + 50, y: self.frame.size.height - batTex.size().height / 2 - 90 - pipeOffset)
+             bat.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: bat.size.width - 40, height: bat.size.height - 20))
+        }
+        
+        // move
+        let moveaction = SKAction.moveBy(x: -self.frame.width - 300, y: 0, duration: 6)
+        bat.run(moveaction)
+        
+        // scale
+        var scaleValue: CGFloat = 0.3
+        
+        let scaleRandom = arc4random() % UInt32(5)
+        if scaleRandom == 1 { scaleValue = 0.9 }
+        else  if scaleRandom == 2 { scaleValue = 0.6 }
+        else if scaleRandom == 3 { scaleValue = 0.8 }
+        else if scaleRandom == 4 { scaleValue = 0.7 }
+        else if scaleRandom == 0 { scaleValue = 1.0 }
+        
+         bat.setScale(scaleValue)
+        
+        let movementRandom = arc4random() % 9
+        if movementRandom == 0 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 + 220, duration: 4)
+        } else if movementRandom == 1 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 - 220, duration: 5)
+        } else if movementRandom == 2 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 - 150, duration: 4)
+        } else if movementRandom == 3 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 + 150, duration: 5)
+        } else if movementRandom == 4 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 + 50, duration: 4)
+        } else if movementRandom == 5 {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2 - 50, duration: 5)
+        } else {
+            moveBatY = SKAction.moveTo(y: self.frame.height / 2, duration: 4)
+        }
+        
+        bat.run(moveBatY)
+        
+        bat.physicsBody?.restitution = 0
+        bat.physicsBody?.isDynamic = false
+        bat.physicsBody?.categoryBitMask = objectGroup
+        bat.zPosition = 1
+        movingObject.addChild(bat)
+    }
+    
+    func timerFunc() {
+        timerAddCoin.invalidate()
+        timerAddRedCoin.invalidate()
+        timerAddBoneTimer.invalidate()
+        
+        timerAddCoin = Timer.scheduledTimer(timeInterval: 2.64, target: self, selector: #selector(GameScene.addCoin), userInfo: nil, repeats: true)
+        timerAddRedCoin = Timer.scheduledTimer(timeInterval: 8.246, target: self, selector: #selector(GameScene.redCoinAdd), userInfo: nil, repeats: true)
+        timerAddBoneTimer = Timer.scheduledTimer(timeInterval: 5.236, target: self, selector: #selector(GameScene.addBat), userInfo: nil, repeats: true)
+    }
+    
+    func stopGameObject() {
+        coinObject.speed = 0
+        redCoinObject.speed = 0
+        movingObject.speed = 0
+        heroObject.speed = 0
+    }
+    
+    func reloadGame() {
+        death = false
+        coinObject.removeAllChildren()
+        redCoinObject.removeAllChildren()
+        
+        scene?.isPaused = false
+        
+        movingObject.removeAllChildren()
+        heroObject.removeAllChildren()
+        
+        coinObject.speed = 1
+        heroObject.speed = 1
+        movingObject.speed = 1
+        bgObject.isPaused = false
+        self.scene?.speed = 1
+        
+        createGround()
+        createSky()
+        createHero()
+        createHeroemitter()
+        
+        timerAddCoin.invalidate()
+        timerAddRedCoin.invalidate()
+        timerAddBoneTimer.invalidate()
+        
+        timerFunc()
     }
     
     override func didFinishUpdate() {
--- Night forest/GameScene_Physics.swift
+++ Night forest/GameScene_Physics.swift
@@ -12,18 +12,81 @@ import  SpriteKit
 extension GameScene {
     func didBegin(_ contact: SKPhysicsContact) {
         
+        if contact.bodyA.categoryBitMask == objectGroup || contact.bodyB.categoryBitMask == objectGroup {
+            death = true
+            hero.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            
+            animations.shakeAndFlashAnimation(view: self.view!)
+            
+            if sound {
+                run(batDeadPreload)
+            }
+            
+            hero.physicsBody?.allowsRotation = false
+            
+            heroemitterObject.removeAllChildren()
+//            coinObject.removeAllChildren()
+//            redCoinObject.removeAllChildren()
+//            movingObject.removeAllChildren()
+            
+//            stopGameObject()
+            
+            timerAddBoneTimer.invalidate()
+            timerAddCoin.invalidate()
+            timerAddRedCoin.invalidate()
+            
+            heroDeathTexturesArray = [
+            SKTexture(imageNamed: "ko_000.png"), SKTexture(imageNamed: "ko_001.png"), SKTexture(imageNamed: "ko_002.png"), SKTexture(imageNamed: "ko_003.png"), SKTexture(imageNamed: "ko_004.png"), SKTexture(imageNamed: "ko_005.png"), SKTexture(imageNamed: "ko_006.png"), SKTexture(imageNamed: "ko_007.png"), SKTexture(imageNamed: "ko_008.png"), SKTexture(imageNamed: "ko_009.png"), SKTexture(imageNamed: "ko_010.png"), SKTexture(imageNamed: "ko_011.png"), SKTexture(imageNamed: "ko_012.png"), SKTexture(imageNamed: "ko_013.png"), SKTexture(imageNamed: "ko_014.png"), SKTexture(imageNamed: "ko_015.png"), SKTexture(imageNamed: "ko_016.png"), SKTexture(imageNamed: "ko_017.png"), SKTexture(imageNamed: "ko_018.png"), SKTexture(imageNamed: "ko_019.png"), SKTexture(imageNamed: "ko_020.png"), SKTexture(imageNamed: "ko_021.png"), SKTexture(imageNamed: "ko_022.png"), SKTexture(imageNamed: "ko_023.png"), SKTexture(imageNamed: "ko_024.png"), SKTexture(imageNamed: "ko_025.png"), SKTexture(imageNamed: "ko_026.png"), SKTexture(imageNamed: "ko_027.png"), SKTexture(imageNamed: "ko_028.png"), SKTexture(imageNamed: "ko_029.png")
+            ]
+            hero.size.height = 120
+            hero.size.width = 130
+            hero.position = CGPoint(x: self.size.width - 100, y: 0)
+            let heroDeathAnimation = SKAction.animate(with: heroDeathTexturesArray, timePerFrame: 0.03)
+            hero.run(heroDeathAnimation)
+            
+        }
         
         if contact.bodyA.categoryBitMask == groundGroup || contact.bodyB.categoryBitMask == groundGroup {
+            if death {
+                bgObject.isPaused = true
+                hero.texture = SKTexture(imageNamed: "ko_029.png")
+                hero.speed = 0
+                hero.removeAllChildren()
+               
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.gameViewControllerBridge.refreshGameButton.isHidden = false
+                }
+           
+            } else {
+                heroEmitter.isHidden = true
+                heroRunTexturesArray = [
+                SKTexture(imageNamed: "r_000.png"), SKTexture(imageNamed: "r_001.png"), SKTexture(imageNamed: "r_002.png"), SKTexture(imageNamed: "r_003.png"), SKTexture(imageNamed: "r_004.png"), SKTexture(imageNamed: "r_005.png"), SKTexture(imageNamed: "r_006.png"), SKTexture(imageNamed: "r_007.png"), SKTexture(imageNamed: "r_008.png"), SKTexture(imageNamed: "r_009.png"), SKTexture(imageNamed: "r_010.png"), SKTexture(imageNamed: "r_011.png"), SKTexture(imageNamed: "r_012.png"), SKTexture(imageNamed: "r_013.png"), SKTexture(imageNamed: "r_014.png"), SKTexture(imageNamed: "r_015.png"), SKTexture(imageNamed: "r_016.png"), SKTexture(imageNamed: "r_017.png"), SKTexture(imageNamed: "r_018.png"), SKTexture(imageNamed: "r_019.png"), SKTexture(imageNamed: "r_020.png"), SKTexture(imageNamed: "r_021.png"), SKTexture(imageNamed: "r_022.png"), SKTexture(imageNamed: "r_023.png"), SKTexture(imageNamed: "r_024.png"), SKTexture(imageNamed: "r_025.png"), SKTexture(imageNamed: "r_026.png"), SKTexture(imageNamed: "r_027.png"), SKTexture(imageNamed: "r_028.png"), SKTexture(imageNamed: "r_029.png"), SKTexture(imageNamed: "r_030.png"), SKTexture(imageNamed: "r_031.png"), SKTexture(imageNamed: "r_032.png"), SKTexture(imageNamed: "r_033.png"), SKTexture(imageNamed: "r_034.png"), SKTexture(imageNamed: "r_035.png"), SKTexture(imageNamed: "r_036.png"), SKTexture(imageNamed: "r_037.png"), SKTexture(imageNamed: "r_038.png"), SKTexture(imageNamed: "r_039.png"), SKTexture(imageNamed: "r_040.png"), SKTexture(imageNamed: "r_041.png"), SKTexture(imageNamed: "r_042.png")
+                           ]
+                let heroRunAnimation = SKAction.animate(with: heroRunTexturesArray, timePerFrame: 0.018)
+                let heroRun = SKAction.repeatForever(heroRunAnimation)
+                           
+                hero.run(heroRun)
+            }
+           
+        }
+        
+        if contact.bodyA.categoryBitMask == coinGroup || contact.bodyB.categoryBitMask == coinGroup {
+            let coinNode = contact.bodyA.categoryBitMask == coinGroup ? contact.bodyA.node : contact.bodyB.node
             
-            heroEmitter.isHidden = true
-            heroRunTexturesArray = [
-            SKTexture(imageNamed: "r_000.png"), SKTexture(imageNamed: "r_001.png"), SKTexture(imageNamed: "r_002.png"), SKTexture(imageNamed: "r_003.png"), SKTexture(imageNamed: "r_004.png"), SKTexture(imageNamed: "r_005.png"), SKTexture(imageNamed: "r_006.png"), SKTexture(imageNamed: "r_007.png"), SKTexture(imageNamed: "r_008.png"), SKTexture(imageNamed: "r_009.png"), SKTexture(imageNamed: "r_010.png"), SKTexture(imageNamed: "r_011.png"), SKTexture(imageNamed: "r_012.png"), SKTexture(imageNamed: "r_013.png"), SKTexture(imageNamed: "r_014.png"), SKTexture(imageNamed: "r_015.png"), SKTexture(imageNamed: "r_016.png"), SKTexture(imageNamed: "r_017.png"), SKTexture(imageNamed: "r_018.png"), SKTexture(imageNamed: "r_019.png"), SKTexture(imageNamed: "r_020.png"), SKTexture(imageNamed: "r_021.png"), SKTexture(imageNamed: "r_022.png"), SKTexture(imageNamed: "r_023.png"), SKTexture(imageNamed: "r_024.png"), SKTexture(imageNamed: "r_025.png"), SKTexture(imageNamed: "r_026.png"), SKTexture(imageNamed: "r_027.png"), SKTexture(imageNamed: "r_028.png"), SKTexture(imageNamed: "r_029.png"), SKTexture(imageNamed: "r_030.png"), SKTexture(imageNamed: "r_031.png"), SKTexture(imageNamed: "r_032.png"), SKTexture(imageNamed: "r_033.png"), SKTexture(imageNamed: "r_034.png"), SKTexture(imageNamed: "r_035.png"), SKTexture(imageNamed: "r_036.png"), SKTexture(imageNamed: "r_037.png"), SKTexture(imageNamed: "r_038.png"), SKTexture(imageNamed: "r_039.png"), SKTexture(imageNamed: "r_040.png"), SKTexture(imageNamed: "r_041.png"), SKTexture(imageNamed: "r_042.png")
-            ]
-            let heroRunAnimation = SKAction.animate(with: heroRunTexturesArray, timePerFrame: 0.018)
-            let heroRun = SKAction.repeatForever(heroRunAnimation)
+            if sound == true {
+                run(pickCoinPreload)
+            }
+            coinNode?.removeFromParent()
+        }
+        
+        if contact.bodyA.categoryBitMask == redCoinGroup || contact.bodyB.categoryBitMask == redCoinGroup {
+            let redCoinNode = contact.bodyA.categoryBitMask == redCoinGroup ? contact.bodyA.node : contact.bodyB.node
             
-            hero.run(heroRun)
+            if sound == true {
+                run(pickCoinPreload)
+            }
             
+            redCoinNode?.removeFromParent()
         }
     }
 }
